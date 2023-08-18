78e20fe4792a678d3338bf5bb0a5b4836c81ecab
monkeymr014 <monkeymr014@gmail.com>
2023-08-18 11:20:33 +0200

    L2 drawing lines

--- lesson/1/main.js
+++ lesson/1/main.js
@@ -4,11 +4,11 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 10, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 'blue' } );
+const geometry = new THREE.BoxGeometry( 1, 1, 3 );
+const material = new THREE.MeshBasicMaterial( { color: 'green' } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
